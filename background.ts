7e1a2816736c811a9ec826dca2b615adb45b01fd
yudai hiyama <yudai1204h@gmail.com>
2023-09-22 23:39:58 +0900

    eslint ignore

--- background.ts
+++ background.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   switch (message.type) {
     case "open_settings":
