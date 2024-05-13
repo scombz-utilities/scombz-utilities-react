4482849c41fedfe61b0119bdbb485affe66801fa
newt239 <ysti2019.win2020.st@gmail.com>
2024-05-14 06:37:52 +0900

    add: show optionId to inputs

--- src/options/components/CustomContainerParent.tsx
+++ src/options/components/CustomContainerParent.tsx
@@ -1,4 +1,4 @@
-import { InputLabel, Stack, FormHelperText } from "@mui/material";
+import { InputLabel, Stack, FormHelperText, Typography } from "@mui/material";
 import React from "react";
 
 type Props = {
@@ -10,11 +10,16 @@ type Props = {
 };
 
 export const CustomContainerParent = (props: Props) => {
-  const { label, caption, htmlFor, children } = props;
+  const { label, caption, htmlFor, optionId, children } = props;
 
   return (
     <Stack gap={1} py={2} borderBottom="1px solid #ccc">
-      <InputLabel htmlFor={htmlFor}>{label}</InputLabel>
+      <Stack sx={{ flexDirection: "row", alignItems: "flex-end", gap: 1 }}>
+        <InputLabel htmlFor={htmlFor}>{label}</InputLabel>
+        <Typography variant="caption" color="grey.500">
+          {optionId}
+        </Typography>
+      </Stack>
       <FormHelperText>{caption}</FormHelperText>
       {children}
     </Stack>
