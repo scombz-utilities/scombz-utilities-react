a2f5efb3dbee436a8e2092edd211587749494143
newt <ysti2019.win2020.st@gmail.com>
2024-05-27 13:20:22 +0900

    feat: remove tabs and add side menu

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,4 +1,16 @@
-import { Alert, Box, Snackbar, Tab, Tabs, ThemeProvider } from "@mui/material";
+import {
+  Alert,
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Snackbar,
+  Stack,
+  ThemeProvider,
+} from "@mui/material";
+import { blue, grey } from "@mui/material/colors";
 import React, { useEffect, useState } from "react";
 import { AdvancedOptions } from "./components/AdvancedOptions";
 import { BasicOptions } from "./components/BasicOptions";
@@ -10,6 +22,8 @@ import { WidgetOptions } from "./components/WidgetOptions";
 import theme from "~/theme";
 import { defaultSaves } from "~settings";
 import type { Saves } from "~settings";
+
+import "./index.css";
 
 const OptionsIndex = () => {
   const [currentTab, setCurrentTab] = useState(new URLSearchParams(window.location.search).get("tab") || "basic");
@@ -47,6 +61,16 @@ const OptionsIndex = () => {
     });
   }, []);
 
+  const menus: { value: string; label: string; icon?: JSX.Element }[] = [
+    { value: "basic", label: chrome.i18n.getMessage("basicOptions") },
+    { value: "widget", label: chrome.i18n.getMessage("widgetOptions") },
+    { value: "layout", label: chrome.i18n.getMessage("layoutOptions") },
+    { value: "advanced", label: chrome.i18n.getMessage("advancedOptions") },
+    { value: "customcss", label: chrome.i18n.getMessage("customCSS") },
+    { value: "data", label: chrome.i18n.getMessage("manageOptions") },
+    { value: "info", label: chrome.i18n.getMessage("info") },
+  ];
+
   return (
     <ThemeProvider theme={theme}>
       <Snackbar
@@ -64,8 +88,21 @@ const OptionsIndex = () => {
           設定を保存しました
         </Alert>
       </Snackbar>
-      <Box p={0} maxWidth={1000} margin="0 auto">
-        <Box pt={2} pb={5} mx={0}>
+      <Stack direction="row" spacing={1}>
+        <Box
+          gap="1rem"
+          py="1rem"
+          sx={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            backgroundColor: "white",
+            borderRightColor: grey[300],
+            borderRightWidth: "1px",
+            borderRightStyle: "solid",
+          }}
+        >
           <Box textAlign="center" mb={2}>
             <img
               src={chrome.runtime.getURL("assets/scombz_utilities.svg")}
@@ -73,59 +110,64 @@ const OptionsIndex = () => {
               style={{ width: "50%", maxWidth: "250px", margin: "0 auto", display: "block" }}
             />
           </Box>
-
-          <Box
-            sx={{
-              borderBottom: 1,
-              borderColor: "divider",
-              position: "sticky",
-              top: 0,
-              left: 0,
-              width: "100%",
-              zIndex: 1,
-              backgroundColor: "#fffa",
-              backdropFilter: "blur(3px)",
-            }}
-          >
-            <Tabs
-              variant="scrollable"
-              scrollButtons="auto"
-              value={currentTab || "basic"}
-              onChange={(_, value) => {
-                setCurrentTab(value);
-                history.pushState(null, "", `?tab=${value}`);
-              }}
-            >
-              <Tab value="basic" label={chrome.i18n.getMessage("basicOptions")} />
-              <Tab value="widget" label={chrome.i18n.getMessage("widgetOptions")} />
-              <Tab value="layout" label={chrome.i18n.getMessage("layoutOptions")} />
-              <Tab value="advanced" label={chrome.i18n.getMessage("advancedOptions")} />
-              <Tab value="customcss" label={chrome.i18n.getMessage("customCSS")} />
-              <Tab value="data" label={chrome.i18n.getMessage("manageOptions")} />
-              <Tab value="info" label={chrome.i18n.getMessage("info")} />
-            </Tabs>
-          </Box>
-
-          <Box py={3}>
-            {(currentTab === "basic" || currentTab === null) && (
-              <BasicOptions saves={currentLocalStorage} setSettings={setSettings} />
-            )}
-            {currentTab === "widget" && <WidgetOptions saves={currentLocalStorage} setSettings={setSettings} />}
-            {currentTab === "layout" && <LayoutOptions saves={currentLocalStorage} setSettings={setSettings} />}
-            {currentTab === "advanced" && (
-              <AdvancedOptions saves={currentLocalStorage} setSettings={setSettings} setScombzData={setScombzData} />
-            )}
-            {currentTab === "customcss" && (
-              <CustomCSS
-                value={currentLocalStorage.settings.customCSS}
-                onSaveButtonClick={(value) => setSettings("customCSS", value)}
-              />
-            )}
-            {currentTab === "data" && <DataOperation saves={currentLocalStorage} setSaves={setCurrentLocalStorage} />}
-            {currentTab === "info" && <Information />}
-          </Box>
+          <List>
+            {menus.map((menu) => (
+              <ListItem
+                disablePadding
+                key={menu.value}
+                onClick={() => {
+                  setCurrentTab(menu.value);
+                  history.pushState(null, "", `?tab=${menu.value}`);
+                }}
+                sx={{
+                  backgroundColor: currentTab === menu.value ? blue[500] : "transparent",
+                  color: currentTab === menu.value ? "white" : "black",
+                  transition: "all 0.3s",
+                  ":hover": {
+                    backgroundColor: currentTab === menu.value ? blue[700] : grey[100],
+                    color: currentTab === menu.value ? "white" : "black",
+                  },
+                }}
+              >
+                <ListItemButton>
+                  {menu.icon && <ListItemIcon>{menu.icon}</ListItemIcon>}
+                  <ListItemText>{menu.label}</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
         </Box>
-      </Box>
+
+        <Box
+          py={3}
+          width="100%"
+          sx={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            width: "calc(100% - 250px - 2rem)",
+            padding: "1rem",
+          }}
+        >
+          {(currentTab === "basic" || currentTab === null) && (
+            <BasicOptions saves={currentLocalStorage} setSettings={setSettings} />
+          )}
+          {currentTab === "widget" && <WidgetOptions saves={currentLocalStorage} setSettings={setSettings} />}
+          {currentTab === "layout" && <LayoutOptions saves={currentLocalStorage} setSettings={setSettings} />}
+          {currentTab === "advanced" && (
+            <AdvancedOptions saves={currentLocalStorage} setSettings={setSettings} setScombzData={setScombzData} />
+          )}
+          {currentTab === "customcss" && (
+            <CustomCSS
+              value={currentLocalStorage.settings.customCSS}
+              onSaveButtonClick={(value) => setSettings("customCSS", value)}
+            />
+          )}
+          {currentTab === "data" && <DataOperation saves={currentLocalStorage} setSaves={setCurrentLocalStorage} />}
+          {currentTab === "info" && <Information />}
+        </Box>
+      </Stack>
     </ThemeProvider>
   );
 };
