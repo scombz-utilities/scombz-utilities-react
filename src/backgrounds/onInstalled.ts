ce84553a84357bc04908009b0aed1b861ff89a8e
yudai hiyama <yudai1204h@gmail.com>
2024-06-19 02:12:13 +0900

    4.1.0でもmigrationを行うようにした

--- src/backgrounds/onInstalled.ts
+++ src/backgrounds/onInstalled.ts
@@ -1,12 +1,8 @@
 import { migrate } from "./migration";
 export const onInstalled = (reason: string) => {
   if (process.env.PLASMO_PUBLIC_ENVIRONMENT === "development") return;
-  if (chrome.runtime.getManifest().version === "4.0.0") {
+  if (chrome.runtime.getManifest().version === "4.1.0") {
     migrate();
-    chrome.tabs.create({
-      url: `https://scombz-utilities.com/lp/4.0.0`,
-      active: true,
-    });
     return;
   }
   if (reason === "install" || reason === "update") {
